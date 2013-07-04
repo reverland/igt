a1efe197c430df2097ebaa025da87cfe953a2e02
Yuyang Liu <lhtlyy@gmail.com>
2013-07-04 18:06:45 +0800

    fix for html output:path and chinese characters

--- metagoofil-blackhat/htmlExport.py
+++ metagoofil-blackhat/htmlExport.py
@@ -2,23 +2,24 @@
 # -*- coding: utf-8 -*-
 from lib import markup
 from lib import graphs
+import chardet
 
 class htmlExport():
-	def __init__(self,users,softs,paths,allinfo,fname,dirs,failed,domain,emails):
-		self.users=users
-		self.softs=softs
-		self.paths=paths
-		self.allinfo=allinfo
-		self.fname=fname
-		self.dir=dirs
-		self.failed=failed
-		self.style=""
-		self.domain=domain
-		self.emails=emails
+    def __init__(self,users,softs,paths,allinfo,fname,dirs,failed,domain,emails):
+        self.users=users
+        self.softs=softs
+        self.paths=paths
+        self.allinfo=allinfo
+        self.fname=fname
+        self.dir=dirs
+        self.failed=failed
+        self.style=""
+        self.domain=domain
+        self.emails=emails
 
-	def styler(self):
-		a="""<style type='text/css'>body {
-             background: #e1e5e4  top no-repeat; 
+    def styler(self):
+        a="""<style type='text/css'>body {
+             background: #e1e5e4  top no-repeat;
          }
 
         h1 { font-family: times, Times New Roman, times-roman, georgia, serif;
@@ -77,71 +78,77 @@ class htmlExport():
         max-height: 300px;
         }
         </style>
-		"""
-		self.style=a
-			
-	def writehtml(self):
-		page = markup.page()
-		page.title("Metagoofil results")
-		page.html()
-		self.styler()
-		page.head(self.style)
-		page.head.close()
-		page.body()
-		page.h2("Metagoofil results")
-		page.h3("Results for: " + self.domain)
-		graph = graphs.BarGraph('vBar')
-		graph.values = [len(self.users),len(self.softs),len(self.emails),len(self.paths)]
-		graph.labels = ["Usernames","Software","Emails","Paths/Servers"]
-		graph.showValues = 1
-		page.body(graph.create())
-		page.h3("User names found:")
-		page.ul( class_="userslist")
-		page.li( self.users, class_="useritem")
-		page.ul.close( )
-		page.h3("Software versions found:")
-		page.ul( class_="softlist")
-		page.li(self.softs, class_="softitem")
-		page.ul.close( )
-		page.h3("E-mails found:")
-		if self.emails!=[]:
-			page.ul( class_="emailslist")
-			page.li(self.emails, class_="emailitem")
-			page.ul.close( )
-		else:
-			page.p("0 results")
-		page.h3("Servers and paths found:")
-		if self.paths!=[]:
-			page.ul( class_="pathslist")
-			page.li(self.paths, class_="pathitem")
-			page.ul.close( )
-		else:
-			page.p("0 results")
-		page.h3("Files analyzed:")
-		page.ul( class_="files")
-		for x in self.allinfo:
-			page.li(x[0], class_="file")
-		page.ul.close()
-		page.h2("Files and metadata found:")
-		for x in self.allinfo:
-			page.h3(x[0])
-			page.a("Local copy", class_="link", href=self.dir+"/"+x[0])
-			page.pre(x[1])
-			page.pre(x[2])
-			page.pre(x[3])
-			page.pre(x[5])
-			page.pre.close()
-		page.h2("Failed extractions and reasons")		
-		for x in self.failed:
-			page.pre(x)
-		page.body.close()
-		page.html.close()
-		file = open(self.fname,'w')
-		for x in page.content:
-			try:
-				file.write(x)
-			except:
-				#print "Exception" +  x # send to logs
-				pass
-		file.close
-		return "ok"
+        """
+        self.style=a
+
+    def writehtml(self):
+        page = markup.page()
+        page.title("Metagoofil results")
+        page.html()
+        self.styler()
+        page.head(self.style)
+        page.head.close()
+        page.body()
+        page.h2("Metagoofil results")
+        page.h3("Results for: " + self.domain)
+        graph = graphs.BarGraph('vBar')
+        graph.values = [len(self.users),len(self.softs),len(self.emails),len(self.paths)]
+        graph.labels = ["Usernames","Software","Emails","Paths/Servers"]
+        graph.showValues = 1
+        page.body(graph.create())
+        page.h3("User names found:")
+        page.ul( class_="userslist")
+        page.li( self.users, class_="useritem")
+        page.ul.close( )
+        page.h3("Software versions found:")
+        page.ul( class_="softlist")
+        page.li(self.softs, class_="softitem")
+        page.ul.close( )
+        page.h3("E-mails found:")
+        if self.emails!=[]:
+            page.ul( class_="emailslist")
+            page.li(self.emails, class_="emailitem")
+            page.ul.close( )
+        else:
+            page.p("0 results")
+        page.h3("Servers and paths found:")
+        if self.paths!=[]:
+            page.ul( class_="pathslist")
+            page.li(self.paths, class_="pathitem")
+            page.ul.close( )
+        else:
+            page.p("0 results")
+        page.h3("Files analyzed:")
+        page.ul( class_="files")
+        for x in self.allinfo:
+            page.li(x[0], class_="file")
+        page.ul.close()
+        page.h2("Files and metadata found:")
+        for x in self.allinfo:
+            page.h3(x[0])
+            page.a("Local copy", class_="link", href=self.dir+"/"+x[0].split('/')[-1])
+            for i in range(len(x[1])):
+                x[1][i] = x[1][i].decode(chardet.detect(x[1][i])['encoding']).encode('utf8')
+            for i in range(len(x[2])):
+                x[2][i] = x[2][i].decode(chardet.detect(x[2][i])['encoding']).encode('utf8')
+            page.pre(x[1])
+            page.pre(x[2])
+            for i in range(len(x[3])):
+                x[3][i] = x[3][i].decode(chardet.detect(x[3][i])['encoding']).encode('utf8')
+            page.pre(x[3][i])
+            page.pre(x[5])
+            page.pre.close()
+        page.h2("Failed extractions and reasons")
+        for x in self.failed:
+            page.pre(x)
+        page.body.close()
+        page.html.close()
+        file = open(self.fname,'w')
+        for x in page.content:
+            try:
+                file.write(x)
+            except:
+                #print "Exception" +  x # send to logs
+                pass
+        file.close
+        return "ok"
